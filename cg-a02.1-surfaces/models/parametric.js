4aa27635b97081e87e72307f48d6b3b1cf640907
Timo Raschke <timo.raschke@gmail.com>
2015-11-18 14:50:55 +0100

    [A02.1] Parametric work..

--- cg-a02.1-surfaces/models/parametric.js
+++ cg-a02.1-surfaces/models/parametric.js
@@ -14,8 +14,29 @@ define(["three"],
 
         var ParametricSurface = function (posFunc, config) {
 
-            this.positions = undefined;
-            this.colors = undefined;
+            var umin = config.umin;
+            var umax = config.umax;
+            var vmin = config.vmin;
+            var vmax = config.vmax;
+            var uSegments = config.uSegments;
+            var vSegments = config.vSegments;
+
+            this.positions = new Float32Array(uSegments * vSegments);
+            this.colors = new Float32Array(uSegments * vSegments);
+
+            var color = new THREE.Color();
+
+            for (var i = 0; i < uSegments; i++) {
+                var u = Math.floor(((umax - umin) / uSegments) * i);
+                for (var j = 0; j < vSegments; j++) {
+                    var v = Math.floor(((vmax - vmin) / uSegments) * j);
+                    var x = Math.sin(u) * Math.sin(v);
+                    var y = Math.cos(u) * Math.sin(v);
+                    var z = Math.cos(v);
+                    var position = [x, y, z];
+                    this.positions[i * j] = position;
+                }
+            }
 
             this.getPositions = function() {
                 return this.positions;
