1ad07a6d614dbe868a0e80761b837580baa30eb7
Timo Raschke <timo.raschke@gmail.com>
2016-01-07 19:36:34 +0100

    [A03.1] filled ShaderMaterial in planet.js like in the lecture

--- cg-a03.1-shaders/models/planet.js
+++ cg-a03.1-shaders/models/planet.js
@@ -16,13 +16,37 @@ define(["three", "shaders"],
             // implement ShaderMaterial using the code from
             // the lecture
 
+            //TODO Make shader compile
             var material = new THREE.ShaderMaterial( {
-                //TODO Implement
+                uniforms: THREE.UniformsUtils.merge( [
+                    THREE.UniformsLib['lights'],
+                    {
+                        diffuseMaterial: {
+                            type: 'c',
+                            value: new THREE.Color(1, 0, 0)
+                        },
+                        specularMaterial: {
+                            type: 'c',
+                            value: new THREE.Color(0.7, 0.7, 0.7)
+                        },
+                        ambientMaterial: {
+                            type: 'c',
+                            value: new THREE.Color(0.8, 0.2, 0.2)
+                        },
+                        shininessMaterial: {
+                            type: 'f',
+                            value: 16.0
+                        }
+                    }
+                ]),
+                vertexShader: Shaders.getVertexShader('planet'),
+                fragmentShader: Shaders.getFragmentShader('planet'),
+                lights: true
             });
             
             // hint:
             // texture can be assigned only when it is loaded completely, e.g. like this
-            // material.uniforms.daytimeTexture.value   = textureName;
+            // material.uniforms.daytimeTexture.value = textureName;
             
             scope.mesh = new THREE.Mesh( new THREE.SphereGeometry(400, 100,100), material);
             scope.mesh.name = "planet";
